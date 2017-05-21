910696f122a4c63b8c6ab8967d0618cc1ab3ca40
Coolly <NineSay@qq.com>
2017-05-22 03:29:02 +0800

    引入工具类
    这边需要一个全局唯一计数器
    用于每一个元素的区分

--- src/pages/new/new.ts
+++ src/pages/new/new.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { ToolService } from '../../app/tool.service';
 /**
  * Generated class for the NewPage page.
  *
